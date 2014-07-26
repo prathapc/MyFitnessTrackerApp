e717a0f843a06d726973bef6d4f69d1a06c81f82
prathapc <prathap.choudhury@gmail.com>
2014-07-26 10:46:00 +0530

    sqldialect updated and schema creted successfully in mysql DB after running using tomcat

--- src/main/java/com/pluralsight/model/Goal.java
+++ src/main/java/com/pluralsight/model/Goal.java
@@ -1,11 +1,16 @@
 package com.pluralsight.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import org.hibernate.validator.constraints.Range;
 
-
-
+@Entity
 public class Goal {
-
+	@Id
+	@GeneratedValue
+	private long id;
 	@Range(min = 1, max = 120)
 	private int minutes;
 
@@ -17,4 +22,11 @@ public class Goal {
 		this.minutes = minutes;
 	}
 	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 }
